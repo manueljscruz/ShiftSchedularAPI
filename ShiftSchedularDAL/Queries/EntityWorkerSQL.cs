588bea1a192073e661278f9b125ba2b2f7765fd0
manueljscruz <manueljscruz@hotmail.com>
2024-04-07 18:07:54 +0100

    Minor adjustments

--- ShiftSchedularDAL/Queries/EntityWorkerSQL.cs
+++ ShiftSchedularDAL/Queries/EntityWorkerSQL.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShiftSchedularDAL.Queries
+﻿namespace ShiftSchedularDAL.Queries
 {
     public class EntityWorkerSQL
     {
+        /// <summary>
+        /// Gets related records between the entity and worker tables
+        /// Where the worker is active
+        /// Currently being used to populate the navbar
+        /// </summary>
         public static readonly string GetEntityWorkersByWorkerId = @"
             SELECT 
                 et.EntityId,
                 et.EntityName, 
                 et.EntityDescription,
-                etw.ActiveWorkerStatus,
                 etw.CanCreateSchedules,
                 etw.IsOwner 
             FROM Entities et
             LEFT JOIN EntityWorkers etw on et.EntityId = etw.EntityId
-            WHERE etw.WorkerId = @WorkerId";
+            WHERE 
+                etw.ActiveWorkerStatus = 1
+                AND etw.WorkerId = @WorkerId";
     }
 }
--- ShiftSchedularEntity/Models/DataTransferObjects/EntityWorkerDTO.cs
+++ ShiftSchedularEntity/Models/DataTransferObjects/EntityWorkerDTO.cs
@@ -7,7 +7,6 @@ namespace ShiftSchedularEntity.Models.DataTransferObjects
         public string EntityId { get; set; }
         public string EntityName { get; set; }
         public string EntityDescription { get; set; }
-        public bool ActiveWorkerStatus { get; set; }
         public bool CanCreateSchedules { get; set; }
         public bool IsOwner { get; set; }
     }
